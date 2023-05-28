2f63fe6c33613a6e7f5be30d9e229dd837726e93
wilsb8 <wilsb8@gmail.com>
2023-05-28 10:06:10 -0700

    Moved a JS script to the top of the HTML

--- public/assets/js/contactForm.js
+++ public/assets/js/contactForm.js
@@ -2,36 +2,37 @@ const form = document.querySelector('.php-email-form');
 const errorDiv = document.querySelector('.error-message');
 const successDiv = document.querySelector('.sent-message');
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  try {
-    const formData = new FormData(form);
-    const response = await fetch('/', {
-      method: 'POST',
-      body: formData,
-      redirect: 'manual', // Prevent automatic redirect
-    });
+  const formData = new FormData(form);
+  fetch('/', {
+    method: 'POST',
+    body: formData,
+    redirect: 'manual', // Prevent automatic redirect
+  })
+    .then((response) => {
+      if (response.ok) {
+        // Show success message
+        successDiv.style.display = 'block';
+        errorDiv.style.display = 'none';
+        form.reset(); // Clear the form
 
-    if (response.ok) {
-      // Show success message
-      successDiv.style.display = 'block';
-      errorDiv.style.display = 'none';
-      form.reset(); // Clear the form
-
-      // Optionally, scroll to the success message
-      successDiv.scrollIntoView({ behavior: 'smooth' });
-    } else {
-      const data = await response.json();
-      // Show error message from the JSON response
+        // Optionally, scroll to the success message
+        successDiv.scrollIntoView({ behavior: 'smooth' });
+      } else {
+        return response.json().then((data) => {
+          // Show error message from the JSON response
+          errorDiv.style.display = 'block';
+          successDiv.style.display = 'none';
+          errorDiv.textContent = data.error;
+        });
+      }
+    })
+    .catch((error) => {
+      // Show error message
       errorDiv.style.display = 'block';
       successDiv.style.display = 'none';
-      errorDiv.textContent = data.error;
-    }
-  } catch (error) {
-    // Show error message
-    errorDiv.style.display = 'block';
-    successDiv.style.display = 'none';
-    errorDiv.textContent = 'An error occurred while sending the message.';
-  }
+      errorDiv.textContent = 'An error occurred while sending the message.';
+    });
 });
